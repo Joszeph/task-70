749c18884b201b8b5ce5c3839fd636067ec2d65d
Yosif Georgiev <iodesign@live.com>
2023-01-09 19:46:28 +0200

    FIX: avatar

--- src/js/app.js
+++ src/js/app.js
@@ -10,10 +10,19 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 
-const imageDiv = document.querySelector('.image')
+const main = document.querySelector('.main');
 
-imageDiv.addEventListener('click', getBigger)
+const imageDiv = document.createElement('div');
+imageDiv.classList.add('image');
 
-function getBigger(){
-  imageDiv.style.transform = 'scale(2)';
-}
+const img = document.createElement('img');
+img.src = 'https://images.pexels.com/users/avatars/157994802/mathilde-langevin-554.jpeg?auto=compress&fit=crop&h=130&w=130&dpr=1';
+
+imageDiv.appendChild(img);
+main.appendChild(imageDiv);
+
+const image = document.querySelector('.image');
+
+image.addEventListener('click', () => {
+  image.style.transform = 'scale(2)';
+});
